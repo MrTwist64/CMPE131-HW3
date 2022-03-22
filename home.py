f042d1b429e16e56c05cad4277c4c26d4643aa36
Aaron Fleming <ercreeper@gmail.com>
2022-03-21 19:30:36 -0700

    Removed comma

--- home.py
+++ home.py
@@ -13,7 +13,7 @@ def home():
         <title>CMPE 131 HW 3</title>
     </head>
     <body>
-        <h1>Welcome, {name}!</h1>
+        <h1>Welcome {name}!</h1>
         <a href="www.google.com">not google</a>
         <ul>"""
     for city in city_names:
